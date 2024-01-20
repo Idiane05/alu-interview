a2a1ee99bb2bfc759acaaec0645540a34400c0b9
Diane INGABIRE <127386345+Idiane05@users.noreply.github.com>
2024-01-20 19:50:53 +0200

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python3
+"""Minimum Operations"""
+import math
 
-"""
-This script defines a function minOperations to calculate
-the fewest number of operations needed to result in exactly n H characters.
-"""
 
 def minOperations(n):
-    """
-    Calculate the fewest number of operations needed to reach exactly n 'H' characters.
-
-    Args:
-        n (int): The target number of 'H' characters.
-
-    Returns:
-        int: The minimum number of operations.
-    """
+    """Minimum Operations"""
     if n <= 1:
         return 0
 
-    result = float('inf')
-    for i in range(2, n + 1):
-        if n % i == 0:
-            operations = minOperations(n // i) + i
-            result = min(result, operations)
+    operations = 0
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
 
-    return result
+    if n > 1:
+        operations += n
 
-# Example usage
-n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    return operations
